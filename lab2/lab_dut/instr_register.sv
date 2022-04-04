267b2d282124a8ffccd58326daa0c7ab042c921f
roxanadimanescu <dimanescuroxana15@gmail.com>
2022-04-04 13:34:29 +0300

    Laborator 6

--- lab2/lab_dut/instr_register.sv
+++ lab2/lab_dut/instr_register.sv
@@ -29,7 +29,15 @@ import instr_register_pkg::*;  // user-defined types are defined in instr_regist
         iw_reg[i] = '{opc:ZERO,default:0};  // reset to all zeros
     end
     else if (load_en) begin
-      iw_reg[write_pointer] = '{opcode,operand_a,operand_b};
+      case(opcode)
+        PASSA : iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_a};
+        PASSB : iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_b};
+        ADD : iw_reg[write_pointer] = '{opcode, operand_a, operand_b, $signed(operand_a + operand_b)};
+        SUB : iw_reg[write_pointer] = '{opcode, operand_a, operand_b, $signed(operand_a - operand_b)};
+        MULT : iw_reg[write_pointer] = '{opcode, operand_a, operand_b, $signed(operand_a * operand_b)};
+        DIV : iw_reg[write_pointer] = '{opcode, operand_a, operand_b, $signed(operand_a / operand_b)};
+        MOD : iw_reg[write_pointer] = '{opcode, operand_a, operand_b, $signed(operand_a % operand_b)};
+      endcase
     end
 
   // read from the register
--- lab2/test/instr_register_test.sv
+++ lab2/test/instr_register_test.sv
@@ -4,7 +4,8 @@
  * with constrained random test generation, functional coverage, and
  * a scoreboard for self-verification.
  **********************************************************************/
-
+import instr_register_pkg::*;
+ `include "instr_register_class.sv"
 module instr_register_test
   import instr_register_pkg::*;  // user-defined types are defined in instr_register_pkg.sv
   
@@ -20,81 +21,18 @@ module instr_register_test
    //input  instruction_t  instruction_word
    tb_ifc.TEST lab2_if
   );
-
+  
   //timeunit 1ns/1ns;
 
-  int seed = 555; //genereaza valorii aleatoare cu reproducere a acelor nr
+  
+initial begin
+  instr_register_class fs;
+  fs=new(lab2_if);
+  fs.run();
+end
 
-  initial begin
-    $display("\n\n***********************************************************");
-    $display(    "***  THIS IS NOT A SELF-CHECKING TESTBENCH (YET).  YOU  ***");
-    $display(    "***  NEED TO VISUALLY VERIFY THAT THE OUTPUT VALUES     ***");
-    $display(    "***  MATCH THE INPUT VALUES FOR EACH REGISTER LOCATION  ***");
-    $display(    "***********************************************************");
-    $display( "First head");
-    $display("\nReseting the instruction register...");
 
-    lab2_if.cb.write_pointer  <= 5'h00;         // initialize write pointer
-    lab2_if.cb.read_pointer   <= 5'h1F;         // initialize read pointer
-    lab2_if.cb.load_en        <= 1'b0;          // initialize load control line
-    lab2_if.cb.reset_n       <= 1'b0;          // assert reset_n (active low)
-    repeat (2) @( lab2_if.cb) ;     // hold in reset for 2 clock cycles
-    lab2_if.cb.reset_n        <= 1'b1;          // deassert reset_n (active low)
-
-    $display("\nWriting values to register stack...");
-    @(lab2_if.cb) lab2_if.cb.load_en <= 1'b1;  // enable writing to register
-    repeat (10) begin
-      @( lab2_if.cb) randomize_transaction; // functia are timp de simulare 0
-      @(negedge lab2_if.cb) print_transaction;
-    end
-    @( lab2_if.cb) lab2_if.cb.load_en <= 1'b0;  // turn-off writing to register
-
-    // read back and display same three register locations
-    $display("\nReading back the same register locations written...");
-    for (int i=0; i<=9; i++) begin
-      // later labs will replace this loop with iterating through a
-      // scoreboard to determine which addresses were written and
-      // the expected values to be read back
-      @( lab2_if.cb) lab2_if.cb.read_pointer <= $unsigned($random)%9;
-      @( negedge lab2_if.cb) print_results;
-    end
-
-    @(lab2_if.cb) ;
-    $display("\n***********************************************************");
-    $display(  "***  THIS IS NOT A SELF-CHECKING TESTBENCH (YET).  YOU  ***");
-    $display(  "***  NEED TO VISUALLY VERIFY THAT THE OUTPUT VALUES     ***");
-    $display(  "***  MATCH THE INPUT VALUES FOR EACH REGISTER LOCATION  ***");
-    $display(  "***********************************************************\n");
-    $finish;
-  end
-
-  function void randomize_transaction;
-    // A later lab will replace this function with SystemVerilog
-    // constrained random values
-    //
-    // The stactic temp variable is required in order to write to fixed
-    // addresses of 0, 1 and 2.  This will be replaceed with randomizeed
-    // write_pointer values in a later lab
-    //
-    static int temp = 0;
-    lab2_if.cb.operand_a     <= $random(seed)%16;                 // between -15 and 15
-    lab2_if.cb.operand_b     <= $unsigned($random)%16;            // between 0 and 15
-    lab2_if.cb.opcode        <= opcode_t'($unsigned($random)%8);  // between 0 and 7, cast to opcode_t type, convesie
-    lab2_if.cb.write_pointer <= temp++; 
-  endfunction: randomize_transaction
-
-  function void print_transaction;
-    $display("Writing to register location %0d: ", lab2_if.cb.write_pointer);
-    $display("  opcode = %0d (%s)", lab2_if.cb.opcode, lab2_if.cb.opcode.name);// opcode.name afiseaza string-ul
-    $display("  operand_a = %0d",   lab2_if.cb.operand_a);
-    $display("  operand_b = %0d\n", lab2_if.cb.operand_b);
-  endfunction: print_transaction
-
-  function void print_results;
-    $display("Read from register location %0d: ", lab2_if.cb.read_pointer); 
-    $display("  opcode = %0d (%s)", lab2_if.cb.instruction_word.opc, lab2_if.cb.instruction_word.opc.name);
-    $display("  operand_a = %0d",   lab2_if.cb.instruction_word.op_a);
-    $display("  operand_b = %0d\n", lab2_if.cb.instruction_word.op_b);
-  endfunction: print_results
 
 endmodule: instr_register_test
+
+  
