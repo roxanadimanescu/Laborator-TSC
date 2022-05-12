427b3e2c890b25e661cb5939e6872aeec1624646
roxanadimanescu <dimanescuroxana15@gmail.com>
2022-05-12 09:41:56 +0300

    delete

--- lab2/work/@_opt/voptjxvhgz
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity instr_register_pkg is
-end instr_register_pkg;
--- lab2/work/instr_register_test/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity instr_register_test is
-end instr_register_test;
